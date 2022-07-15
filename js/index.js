393dda176849b7a81d1ba6b526f16a3940377d9c
RomanNez <80975404357roma@gmail.com>
2022-07-15 13:49:16 +0300

    hesElem

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ let concattArr = arr1.concat(arr2);
 
 // push, unshift
 // 3. Дан массив [1, 2, 3]. Добавьте ему в конец элементы 4, 5, 6.
-arr1 = [1,2,3]
+arr1 = [1,2,3];
 let pushArr = arr1.push(4,5,6);
 
 // console.log(arr1);
@@ -20,13 +20,12 @@ let unshiftArr = arr1.unshift(4,5,6);
 // shift, pop
 // 5. Дан массив ['js', 'css', 'html']. Выведите на экран первый элемент и удалите
 // его.
-
 let arr = ['js', 'css', 'html'];
 console.log(arr.shift(), arr);
 
+
 // 6. Дан массив ['js', 'css', 'html']. Выведите на экран последний элемент и
 // удалите его.
-
 arr = ['js', 'css', 'html'];
 console.log(arr.pop(), arr);
 
@@ -36,26 +35,80 @@ console.log(arr.pop(), arr);
 // элементы [1, 2, 3].
 arr = [1, 2, 3, 4, 5];
 let newArr = arr.slice(0, 3);
-console.log(newArr, arr)
+console.log(newArr, arr);
+
+
 // 8. Дан массив [1, 2, 3, 4, 5]. С помощью метода slice запишите в новый
 // элементы [4, 5].
-newArr = arr.slice(3,5)
+newArr = arr.slice(3,5);
 console.log(newArr, arr);
 
 
 // 9. Дан массив [1, 2, 3, 4, 5]. С помощью метода splice преобразуйте массив
 // в [1, 4, 5].
 arr.splice(1, 2);
-console.log(arr)
+console.log(arr);
+
 
 // 10. Дан массив [1, 2, 3, 4, 5]. С помощью метода splice запишите в новый
 // массив элементы [2, 3, 4].
 arr = [1, 2, 3, 4, 5];
-let newSpliceArr = arr.splice(1, 3)
-console.log(newSpliceArr)
+let newSpliceArr = arr.splice(1, 3);
+console.log(newSpliceArr);
+
 
 // 11. Дан массив [1, 2, 3, 4, 5]. С помощью метода splice сделайте из него
-// массив [1, 2, 3, &#39;a&#39;, &#39;b&#39;, &#39;c&#39;, 4, 5].
+// массив [1, 2, 3, 'a', 'b', 'c', 4, 5].
+arr = [1, 2, 3, 4, 5];
+arr.splice(3, 0, 'a', 'b', 'c');
+console.log(arr)
+
 
 // 12. Дан массив [1, 2, 3, 4, 5]. С помощью метода splice сделайте из него
-// массив [1, &#39;a&#39;, &#39;b&#39;, 2, 3, 4, &#39;c&#39;, 5, &#39;e&#39;].
+// массив [1, 'a', 'b', 2, 3, 4, 'c', 5, 'e'].
+arr = [1, 2, 3, 4, 5];
+arr.splice(5, 0, 'e');
+arr.splice(4, 0, 'c');
+arr.splice(1, 0, 'a', 'b');
+
+//изза чего в одну строку не делает?  arr.splice(5, 0, 'e').splice(4, 0, 'c').splice(1, 0, 'a', 'b')
+console.log(arr)
+
+
+// sort
+// 13. Дан массив [3, 4, 1, 2, 7]. Отсортируйте его.
+arr = [3, 4, 1, 2, 7];
+console.log(arr.sort());
+
+
+
+
+// 1. Необходимо создать функцию hasElem, которая параметрами будет
+// принимать массив и строку, и возвращать true, если строка есть в
+// массиве, и false - если нет
+arr = [2, 4, 'a', 'r'];
+let string = 'a';
+
+const hasElem1 = function(arr=[], string=''){
+  for(elem of arr){
+    if(elem===string){
+      return true
+    }
+  }
+  return false
+}
+console.log(hasElem1(arr, string));
+
+
+const hasElem = function(arr=[], string=''){
+  let rez = false
+  arr.forEach((elem) =>{
+    // console.log(elem)
+    if(elem===string){
+      rez = true
+    }
+  })
+  return rez
+}
+console.log(hasElem(arr, string));
+
